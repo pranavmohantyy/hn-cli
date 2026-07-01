5a2f70ec187ee351dc625baa528da2c47baacd23
pranavmohantyy <mohantypranav147@gmail.com>
2026-06-30 23:45:01 -0700

    add category support for stories

--- fetcher.py
+++ fetcher.py
@@ -1,7 +1,8 @@
 import requests
 
-def fetch_top_story_ids():
-    response = requests.get('https://hacker-news.firebaseio.com/v0/topstories.json')
+
+def fetch_top_story_ids(category):
+    response = requests.get(f'https://hacker-news.firebaseio.com/v0/{category}.json')
     return response.json()
 
 
@@ -11,10 +12,11 @@ def fetch_item_details(item_id):
 
 
 if __name__ == '__main__':
-    top_story_ids = fetch_top_story_ids()
+    import sys
+    category = sys.argv[1] if len(sys.argv) > 1 else 'topstories'
+    top_story_ids = fetch_top_story_ids(category)
     for index, story_id in enumerate(top_story_ids[:30]):
         story_details = fetch_item_details(story_id)
         title = story_details.get('title', 'No Title')
         score = story_details.get('score', 0)
-        comments = story_details.get('descendants', 0)
-        print(f"{index + 1}. {title} (Score: {score}, Comments: {comments})")
+        print(f'{index + 1}. {title} (Score: {score})')
