4acf259e4a80295c91c485c2207a8256dc0911d2
pranavmohantyy <mohantypranav147@gmail.com>
2026-07-01 23:57:55 -0700

    add comment fetching and display

--- fetcher.py
+++ fetcher.py
@@ -11,6 +11,19 @@ def fetch_item_details(item_id):
     return response.json()
 
 
+def fetch_comments(item):
+    comments = item.get('kids', [])
+    return comments
+
+
+def display_comments(comments, depth=0):
+    for comment_id in comments:
+        comment = fetch_item_details(comment_id)
+        print('    ' * depth + f'Comment by {comment.get("by", "")}: {comment.get("text", "")[:50]}...')
+        if 'kids' in comment:
+            display_comments(comment['kids'], depth + 1)
+
+
 if __name__ == '__main__':
     import sys
     category = sys.argv[1] if len(sys.argv) > 1 else 'topstories'
@@ -20,15 +33,13 @@ if __name__ == '__main__':
         top_story_ids = fetch_top_story_ids(category)
         start_index = page * stories_per_page
         end_index = start_index + stories_per_page
-        print("Select a story number:")
-        for i, story_id in enumerate(top_story_ids[start_index:end_index]):
-            print(f'{i + 1}. {story_id}')
-        choice = input()
-        if choice.isdigit() and 1 <= int(choice) <= stories_per_page:
-            item_details = fetch_item_details(top_story_ids[start_index + int(choice) - 1])
-            print(f'Title: {item_details["title"]}\nURL: {item_details["url"]}\nScore: {item_details["score"]}\nAuthor: {item_details["by"]}\nTime: {item_details["time"]}')
-        else:
-            print("Invalid choice. Try again.")
-            continue
-        if input("Continue? (y/n): ").lower() != 'y':
+        for story_id in top_story_ids[start_index:end_index]:
+            story = fetch_item_details(story_id)
+            print(f'Title: {story.get("title", "")}
+URL: {story.get("url", "")}
+')
+            comments = fetch_comments(story)
+            display_comments(comments)
+        page += 1
+        if start_index >= len(top_story_ids):
             break
